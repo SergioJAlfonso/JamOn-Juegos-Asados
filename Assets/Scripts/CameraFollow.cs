93cc7a3b938fd506183162846f8076495b03f575
Sergio Alfonso <sergialf@ucm.es>
2021-04-14 04:15:24 +0200

    Inicios de los moviementos de camara

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,18 +2,19 @@
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject player = null;
-    [SerializeField] float delay = 1;
+    [SerializeField] float smoothDelay = 0.125f;
 
     void FixedUpdate()
     {
         if (player != null)
         {
             // Coge la posición del jugador
-            Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+            Vector3 playerPos = new Vector3(player.transform.position.x, 0, -10);
+
 
             // Si el jugador está activo (vivo), la cámara le sigue en un intervalo de tiempo dado por delay
             if (player.gameObject.activeSelf)
-                transform.position = Vector3.Lerp(transform.position, playerPos, delay);
+                transform.position = Vector3.Lerp(transform.position, playerPos, smoothDelay);
         }
     }
 }
