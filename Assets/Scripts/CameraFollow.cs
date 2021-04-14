625cdf67819a94b36f5a75e7ff2b75105ad81520
Sergio Alfonso <sergialf@ucm.es>
2021-04-14 14:04:02 +0200

    Chekea tremendos bounds de la cámara
    Valores de min bound: x:-2 y:0 z:-10
    max Bound x:2 y:0 z:-10

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,8 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject player = null;
     [SerializeField] float smoothDelay = 0.125f;
+    [SerializeField] Vector3 minBound, maxBound;
+
 
     void FixedUpdate()
     {
@@ -11,10 +13,15 @@ public class CameraFollow : MonoBehaviour
             // Coge la posición del jugador
             Vector3 playerPos = new Vector3(player.transform.position.x, 0, -10);
 
+            //Chekea los bounds
+            Vector3 boundPos = new Vector3(
+                Mathf.Clamp(playerPos.x, minBound.x, maxBound.x),
+                Mathf.Clamp(playerPos.y, minBound.y, maxBound.y),
+                Mathf.Clamp(playerPos.z, minBound.z, maxBound.z));
 
             // Si el jugador está activo (vivo), la cámara le sigue en un intervalo de tiempo dado por delay
             if (player.gameObject.activeSelf)
-                transform.position = Vector3.Lerp(transform.position, playerPos, smoothDelay);
+                transform.position = Vector3.Lerp(transform.position, boundPos, smoothDelay*Time.fixedDeltaTime);
         }
     }
 }
