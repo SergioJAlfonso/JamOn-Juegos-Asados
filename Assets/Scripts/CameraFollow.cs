874ad049d6c7e6bbf198beeb29f5c40cdc23c8ec
Sergio Alfonso <sergialf@ucm.es>
2021-04-17 20:30:55 +0200

    Cascadita ahora mas velos fiauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuun

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject player = null;
     [SerializeField] float smoothDelay = 0.125f;
     [SerializeField] Vector3 minBound, maxBound;
+    public Image panel;
+    bool flashStarted = true;
+    float blancoBienTemporizador = 2.5f;
 
+    [SerializeField] float maxReductionTime = 4;
+    float reductionTime = 0;
+    bool speedBoosted = true;
+
+    private void Start()
+    {
+        Color fixedColor = panel.color;
+        fixedColor.a = 1;
+        panel.color = fixedColor;
+        panel.CrossFadeAlpha(0f, 0f, true);
+
+        //panel.canvasRenderer.SetAlpha(1f);
+        //.CrossFadeAlpha(1, 10f, true);
+    }
 
     void FixedUpdate()
     {
+       
         if (player != null)
         {
             // Coge la posición del jugador
@@ -26,9 +45,19 @@ public class CameraFollow : MonoBehaviour
                 Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation,
                 Quaternion.Euler(-30, 0, 0),
                 Time.deltaTime * 5f);
+
+                if (!flashStarted)
+                {
+                    panel.CrossFadeAlpha(0f, 1f, true);
+                    flashStarted = true;
+                    speedBoosted = true;
+                    blancoBienTemporizador = 2.5f;
+                }
+
             }
             else
             {
+                blancoBienTemporizador -= Time.deltaTime;
                 boundPos = new Vector3(
                     Mathf.Clamp(playerPos.x, minBound.x, maxBound.x),
                     Mathf.Clamp(playerPos.y, -100, 100),
@@ -37,9 +66,45 @@ public class CameraFollow : MonoBehaviour
                 Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation,
                 Quaternion.Euler(30, 0, 0),
                 Time.deltaTime * 5f);
+
+                if (flashStarted)
+                {
+
+                    if (blancoBienTemporizador <= 0)
+                    {
+                        panel.CrossFadeAlpha(1f, 2.5f, true);
+                        flashStarted = false;
+                    }
+                    if (speedBoosted)
+                    {
+                        Vector3 newScale = new Vector3(player.transform.localScale.x, player.transform.localScale.y * 1.5f, player.transform.localScale.z);
+
+                        player.transform.localScale = Vector3.Lerp(transform.localScale, newScale, smoothDelay);
+
+                        GameManager.instance.parallaxMultiplier(5f);
+                        //GameManager.instance.speedMultiplier(1.5f);
+
+                        reductionTime = maxReductionTime;
+
+                        GameManager.instance.setHasToRestore(true);
+                        speedBoosted = false;
+                    }
+                }
+
+
+                if (reductionTime > 0.2/*maxReductionTime/2*/)
+                {
+                    reductionTime -= Time.deltaTime;
+                        Camera.main.fieldOfView += 0.05f;
+                }
+                if (reductionTime > 0.0f)
+                {
+                    reductionTime -= Time.deltaTime;
+                        Camera.main.fieldOfView -= 0.01f;
+                }
+
             }
             //Chekea los bounds
-
 
             // Si el jugador está activo (vivo), la cámara le sigue en un intervalo de tiempo dado por delay
             if (player.gameObject.activeSelf)
