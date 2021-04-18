aea98e080314a906f0796a9d07ad3c500c793af8
diecasti <diecasti@ucm.es>
2021-04-18 22:39:33 +0200

    Oleadas Objetos Guerra

--- Assets/Scripts/CoolCarpaBotFollow.cs
+++ Assets/Scripts/CoolCarpaBotFollow.cs
@@ -7,7 +7,6 @@ public class CoolCarpaBotFollow : MonoBehaviour
     // Start is called before the first frame update
     public Transform target;
 
-    public Transform puntito;
     public float factorX = 1.0f;
     public float amplitudeX = 1.0f;
     public float factorY = 1.0f;
@@ -50,10 +49,7 @@ public class CoolCarpaBotFollow : MonoBehaviour
         destino.z = target.position.z;
 
         destino = target.transform.position + target.transform.InverseTransformPoint(destino);
-        if (puntito != null)
-        {
-            puntito.transform.position = destino;
-        }
+        
         //destino.y = -destino.y;
 
 
