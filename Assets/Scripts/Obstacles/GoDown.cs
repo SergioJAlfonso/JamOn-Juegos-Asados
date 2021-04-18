ecd1ebc71da48668804b0126a8315db2da001402
albemu07 <albemu07@ucm.es>
2021-04-18 12:32:47 +0200

    mal ya no se

--- Assets/Scripts/Obstacles/GoDown.cs
+++ Assets/Scripts/Obstacles/GoDown.cs
@@ -24,14 +24,16 @@ public class GoDown : MonoBehaviour
 
     void Update()
     {
-        if (speed_ != GameManager.instance.getSpeed())
+        if (speed_ != GameManager.instance.getSpeed() && destroyable)
         {
             speed_ = GameManager.instance.getSpeed();
             rb.velocity = new Vector2(0, -speed_);
         }
 
         if (GameManager.instance.getRecovery() && destroyable)
+        {
             Destroy(this.gameObject);
+        }
     }
 
     //public void initialize(int speed)
