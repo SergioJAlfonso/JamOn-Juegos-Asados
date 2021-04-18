a5f51f50c6a0a058653c005d0cd6beb6505d081e
Sergio Alfonso <sergialf@ucm.es>
2021-04-18 23:27:21 +0200

    amarillo pocho

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -320,13 +320,13 @@ public class GameManager : MonoBehaviour
             case 1:
                 newColor = new Color(0, 255, 207, 60);
                 //colorPanel.CrossFadeColor(new Color(255, 255, 255, 1), 0f, true, true);
-                colorPanel.CrossFadeColor(newColor, 7f, true, true);  
+                colorPanel.CrossFadeColor(newColor, 3f, true, true);  
                 break;
             case 2:
                 newColor = new Color(255, 67, 0, 90);
                 //colorPanel.CrossFadeColor(new Color(255, 255, 255, 1), 0f, true, true);
                 //colorPanel.color();
-                colorPanel.CrossFadeColor(new Color(255, 255, 255, 1), 0.2f, true, true);
+                //colorPanel.CrossFadeColor(new Color(255, 255, 255, 1), 0.2f, true, true);
                 colorPanel.CrossFadeColor(newColor, 7f, true, true);
                 rainEffect.SetActive(true);
                 break;
